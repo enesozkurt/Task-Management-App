2f0d051ba95c320e4fcdf202793cf7a55f5bbc84
enesokurt <enes.ozkurt4@gmail.com>
2021-01-05 00:10:27 +0300

    added get and post task requests

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {Task, TaskStatus} from "./tasks.model";
+import * as uuid from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid.v1(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+}
