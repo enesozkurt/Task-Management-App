4ceaa1b1ff0ed90960ff3b39db255b5a20373f4c
enesokurt <enes.ozkurt4@gmail.com>
2021-01-14 21:58:11 +0300

    hashing password

--- dist/tasks/task.repository.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from "typeorm";
-import { Task } from "./task.entity";
-import { CreateTaskDto } from "./dto/create-task-dto";
-import { GetTasksFilterDto } from "./dto/get-tasks-filter-dto";
-export declare class TaskRepository extends Repository<Task> {
-    getTasks(filterDto: GetTasksFilterDto): Promise<Task[]>;
-    createTask(createTaskDto: CreateTaskDto): Promise<Task>;
-}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {UserRepository} from "./user.repository";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([UserRepository])
+    TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
